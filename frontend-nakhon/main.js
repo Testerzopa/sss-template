948c0f5aeed1a9148eac83d922142d57dd67431c
Testerzopa <testerzopa111@gmail.com>
2026-06-21 08:03:16 +0700

    Add files via upload

---
+++ frontend-nakhon/main.js
@@ -0,0 +1,8 @@
+const hamburger = document.getElementById('hamburger');
+  const mobileMenu = document.getElementById('mobileMenu');
+  hamburger.addEventListener('click', () => {
+    mobileMenu.classList.toggle('open');
+  });
+  mobileMenu.querySelectorAll('a').forEach(a => {
+    a.addEventListener('click', () => mobileMenu.classList.remove('open'));
+  });
